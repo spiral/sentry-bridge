eb7fa95be70e899b02d84bf0965b104ec4b2c949
Wolfy-J <wolfy.jd@gmail.com>
2019-11-08 19:47:23 +0300

    - more tests

---
+++ tests/Sentry/BootloaderTest.php
@@ -0,0 +1,55 @@
+<?php
+
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Sentry;
+
+use Mockery as m;
+use PHPUnit\Framework\TestCase;
+use Sentry\Client;
+use Sentry\ClientInterface;
+use Spiral\Boot\BootloadManager;
+use Spiral\Boot\Environment;
+use Spiral\Boot\EnvironmentInterface;
+use Spiral\Config\ConfiguratorInterface;
+use Spiral\Core\Container;
+use Spiral\Sentry\Bootloader\SentryBootloader;
+use Spiral\Sentry\Config\SentryConfig;
+
+class BootloaderTest extends TestCase
+{
+    public function testBootloader(): void
+    {
+        $config = m::mock(ConfiguratorInterface::class);
+
+        $c = new Container();
+        $c->bind(ConfiguratorInterface::class, $config);
+        $c->bind(EnvironmentInterface::class, new Environment([
+            'SENTRY_DSN' => 'test'
+        ]));
+
+        $config->expects('setDefaults')->with('sentry', [
+            'dsn' => 'test'
+        ]);
+
+        (new BootloadManager($c))->bootload([SentryBootloader::class]);
+
+        $c->bind(SentryConfig::class, new SentryConfig([
+            'dsn' => 'https://key@sentry.demo/2'
+        ]));
+
+        $client = $c->get(ClientInterface::class);
+
+        $this->assertInstanceOf(Client::class, $client);
+
+        /** @var Client $client */
+        $this->assertSame('https://sentry.demo', $client->getOptions()->getDsn());
+    }
+}
