affec2999ce7619470e2acd50858a9a5b94d0ca9
Wolfy-J <wolfy.jd@gmail.com>
2019-11-08 19:32:28 +0300

    - initial tests

---
+++ tests/Sentry/ConfigTest.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Spiral Framework.
+ *
+ * @license   MIT
+ * @author    Anton Titov (Wolfy-J)
+ */
+declare(strict_types=1);
+
+
+namespace Spiral\Tests\Sentry;
+
+use PHPUnit\Framework\TestCase;
+use Spiral\Sentry\Config\SentryConfig;
+
+class ConfigTest extends TestCase
+{
+    public function testConfig()
+    {
+        $cfg = new SentryConfig(['dsn' => 'value']);
+        $this->assertSame('value', $cfg->getDSN());
+    }
+}
--- tests/Sentry/SnapshotterTest.php
+++ tests/Sentry/SnapshotterTest.php
@@ -9,9 +9,50 @@ declare(strict_types=1);
 
 namespace Spiral\Tests\Sentry;
 
+use Mockery as m;
 use PHPUnit\Framework\TestCase;
+use Psr\Log\Test\TestLogger;
+use Sentry\ClientInterface;
+use Spiral\Debug\State;
+use Spiral\Sentry\SentrySnapshotter;
 
 class SnapshotterTest extends TestCase
 {
+    public function testLogger()
+    {
+        $logger = new TestLogger();
+
+        $client = m::mock(ClientInterface::class);
+        $client->expects('captureException');
+
+        $sentry = new SentrySnapshotter(
+            $client,
+            null,
+            $logger
+        );
+
+        $sentry->register(new \Error('hello world'));
+        $this->assertTrue($logger->hasErrorRecords());
+    }
+
+    public function testLoggerWithState()
+    {
+        $logger = new TestLogger();
+
+        $client = m::mock(ClientInterface::class);
+        $client->expects('captureException');
+
+        $state = new State();
+        $state->setTag('test', 'tag');
+
+        $sentry = new SentrySnapshotter(
+            $client,
+            $state,
+            $logger
+        );
+
+        $sentry->register(new \Error('hello world'));
+        $this->assertTrue($logger->hasErrorRecords());
+    }
 
 }
